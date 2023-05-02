9099eb1df325a510833e0e42bac70d03bc68bfa2
ec2604 <erachoshen@gmail.com>
2023-05-02 14:44:26 +0300

    Add clear-target material. Modify reacher.xml, reacher.py

--- dm_control/suite/reacher.py
+++ dm_control/suite/reacher.py
@@ -15,6 +15,10 @@
 
 """Reacher domain."""
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import collections
 
 from dm_control import mujoco
@@ -28,7 +32,7 @@ import numpy as np
 
 SUITE = containers.TaggedTasks()
 _DEFAULT_TIME_LIMIT = 20
-_BIG_TARGET = .05
+_BIG_TARGET = .005
 _SMALL_TARGET = .015
 
 
@@ -69,6 +73,12 @@ class Physics(mujoco.Physics):
     """Returns the signed distance between the finger and target surface."""
     return np.linalg.norm(self.finger_to_target())
 
+  def finger_loc(self):
+    return self.named.data.geom_xpos['finger', :2]
+
+  def target_loc(self):
+    return self.named.data.geom_xpos['target', :2]
+
 
 class Reacher(base.Task):
   """A reacher `Task` to reach the target."""
@@ -84,20 +94,35 @@ class Reacher(base.Task):
         automatically (default).
     """
     self._target_size = target_size
-    super().__init__(random=random)
+    super(Reacher, self).__init__(random=random)
+    self.angle = self.random.uniform(0, 2 * np.pi)
+    self.radius = self.random.uniform(.05, .20)
+    self.wrist_pos = 0
+    self.shoulder_pos = 0
 
   def initialize_episode(self, physics):
     """Sets the state of the environment at the start of each episode."""
     physics.named.model.geom_size['target', 0] = self._target_size
-    randomizers.randomize_limited_and_rotational_joints(physics, self.random)
+    # randomizers.randomize_limited_and_rotational_joints(physics, self.random)
+
+    with physics.reset_context():
+      physics.named.data.qpos['shoulder'] = self.shoulder_pos
+      physics.named.data.qpos['wrist'] = self.wrist_pos
 
     # Randomize target position
-    angle = self.random.uniform(0, 2 * np.pi)
-    radius = self.random.uniform(.05, .20)
-    physics.named.model.geom_pos['target', 'x'] = radius * np.sin(angle)
-    physics.named.model.geom_pos['target', 'y'] = radius * np.cos(angle)
+    #self.angle = self.random.uniform(0, 2 * np.pi)
+    #self.radius = self.random.uniform(.05, .20)
+    physics.named.model.geom_pos['target', 'x'] = self.radius * np.sin(self.angle)
+    physics.named.model.geom_pos['target', 'y'] = self.radius * np.cos(self.angle)
 
-    super().initialize_episode(physics)
+    super(Reacher, self).initialize_episode(physics)
+
+    if self.get_sparse_reward(physics) == 1:
+      range_min, range_max = physics.model.jnt_range[1]
+      self.wrist_pos = np.random.uniform(range_min, range_max)
+      self.shoulder_pos = np.random.uniform(-np.pi, np.pi)
+      self.initialize_episode(physics)
+
 
   def get_observation(self, physics):
     """Returns an observation of the state and the target position."""
@@ -107,6 +132,40 @@ class Reacher(base.Task):
     obs['velocity'] = physics.velocity()
     return obs
 
-  def get_reward(self, physics):
+  def get_sparse_reward(self, physics):
     radii = physics.named.model.geom_size[['target', 'finger'], 0].sum()
-    return rewards.tolerance(physics.finger_to_target_dist(), (0, radii))
+    reward = rewards.tolerance(physics.finger_to_target_dist(), (0, radii))
+    #if reward==1:
+    #  print("inner dist is: ", physics.finger_to_target_dist())
+    return reward
+
+  def get_reward(self, physics):
+    reward_dist = - physics.finger_to_target_dist()
+    reward = reward_dist
+    return reward
+
+  def get_state(self,physics):
+    return physics.finger_loc()
+
+  def get_reward_from_state_sparse(self, physics, state):
+    #physics.named.model.geom_pos['finger', 'x'] = state[1]
+    #physics.named.model.geom_pos['finger', 'y'] = state[0]
+    #reward = self.get_sparse_reward(physics)
+    dist = np.linalg.norm(physics.target_loc() - state)
+    radii = physics.named.model.geom_size[['target', 'finger'], 0].sum()
+    reward = rewards.tolerance(dist, (0, radii))
+    #if reward == 1:
+    #  print('target:', physics.target_loc(), 'state:', state)
+    return reward
+
+
+  def get_reward_from_state_dense(self, physics, state):
+    dist = - np.linalg.norm(physics.target_loc() - state)
+    reward = dist
+    return reward
+
+  def print_stuff(self, physics):
+    print('target according to variables:', self.radius * np.cos(self.angle), self.radius * np.sin(self.angle))
+    physics_loc = physics.target_loc()
+    print('target according to geom_xpos:', physics_loc)
+    print('target according to geom_pos:', physics.named.model.geom_pos['target', 'x'], physics.named.model.geom_pos['target', 'y'])
